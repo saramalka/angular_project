5a79ce9308532ef19a06a704995afce2f5e014cd
saramalka <sari.shlomovitz@gmail.com>
2024-12-31 02:39:02 +0200

    add components

--- src/app/modules/home-component/home-component.component.ts
+++ src/app/modules/home-component/home-component.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'home-component',
   standalone: false,
-  
   templateUrl: './home-component.component.html',
   styleUrl: './home-component.component.css'
 })
